fc2b175bc770159a13a01b87ffc26e4e29a046fc
Yanina Rodriguez <rodriguezyanina169@gmail.com>
2023-07-10 13:30:23 -0300

    agrego estructura sobre la cual comenzar a agregar el front

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,30 @@
 import { Container } from "react-bootstrap";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import FormularioTarea from "./components/FormularioTareas";
 import Menu from "./common/Menu";
 import Footer from "./common/Footer";
+import Administrador from "./components/views/Administrador"
+import Inicio from "./components/views/Inicio"
+import Login from "./components/views/Login"
+import Registro from "./components/views/Registro"
+import { BrowserRouter , Routes, Route } from "react-router-dom";
+import ListaTareas from "./components/views/producto/ListaTareas";
 
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Menu></Menu>
-      <Container className="my-5 main">
-        <h1 className="display-4 text-center text-light">Lista de tareas</h1>
-        <hr />
-        <FormularioTarea></FormularioTarea>
-      </Container>
+    <Routes>
+      <Route exact path="./" element={<Inicio></Inicio>}></Route>
+      <Route exact path="./administrador" element={<Administrador></Administrador>}></Route>
+      <Route exact path="./login" element={<Login></Login>}></Route>
+      <Route exact path="./registro" element={<Registro></Registro>}></Route>
+      <Route exact path="./administrador/listatareas" element={<ListaTareas></ListaTareas>}></Route>
+    </Routes>
+     
       <Footer></Footer>
-    </>
+    </BrowserRouter>
   );
 }
 
